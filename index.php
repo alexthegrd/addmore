792b49bbececaad8c9a0cf1a36d604180a51bb77
AlexG <alekosg@icloud.com>
2022-12-06 13:11:24 +0200

    fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADDMdsddsdsORE</title>
+    <title>ADDMORE</title>
     <link rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.1/css/bootstrap.min.css' />
 </head>
