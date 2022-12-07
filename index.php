9a0258d3b2593b85e079b3347b3f219b6762a98d
AlexG <alekosg@icloud.com>
2022-12-07 10:27:37 +0200

    last

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADDMORE</title>
+    <title>ADDMORgggE</title>
     <link rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.1/css/bootstrap.min.css' />
 </head>
